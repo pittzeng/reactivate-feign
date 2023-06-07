f9cb67b78c0f2fec8eb563b52789615fd2ae9ad6
pittzeng <276488035@qq.com>
2023-06-07 23:32:08 +0000

    Maven artifacts for reactivate-feign-1.0.0

--- src/main/java/com/yigou/common/feign/FeignApplication.java
+++
@@ -1,7 +0,0 @@
-package com.yigou.common.feign;
-
-public class FeignApplication {
-   /* public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }*/
-}
--- src/main/java/com/yigou/common/feign/handler/RestHandler.java
+++
@@ -1,9 +0,0 @@
-package com.yigou.common.feign.handler;
-
-import com.yigou.common.feign.bean.RequestParamInfo;
-import org.reactivestreams.Publisher;
-
-public interface RestHandler {
-
-    Publisher<?> invoke(RequestParamInfo requestParamInfo);
-}
